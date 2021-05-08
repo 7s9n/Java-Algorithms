cfe41351bf1ec7c95f6af6a0743dc63d82ed4dd3
Hussein Sarea <zzzsssx0@gmail.com>
2021-05-08 04:45:15 +0300

    commit

--- src/Maths/Mean.java
+++ src/Maths/Mean.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Mean {
     public static void main (String[] args) {
         System.out.println(getMean(new int[]{1 , 2 , 2}));
+        System.out.println(getMean(3, 4, 6, 6, 8, 9, 11));
     }
     public static double getMean(int... values){
         double sum = Arrays.stream(values).sum();
