26208cca21a2cb4ebe8330e37068b0164eeb6e23
Hussein Sarea <zzzsssx0@gmail.com>
2021-05-08 06:24:15 +0300

    commit

--- src/Maths/Median.java
+++ src/Maths/Median.java
@@ -11,14 +11,17 @@ public class Median {
         assert getMedian(new int[] {1, 2, 3, 4, 5, 6, 8, 9}) == 4.5;
 
         System.out.println(getMedian(new int[]{1, 2, 3, 4, 5, 6, 8, 9}));
+        System.out.println(getMedian(3 , 2 ,0 ,8,7 ,1));
     }
     /**
-     * Calculate average median
-     *
+     * Method to compute the median from a set of numerical values.
      * @param values number series
      * @return median of given {@code values}
      */
-    public static double getMedian(int[] values){
+    public static double getMedian(int... values){
+        if (values.length <= 1){
+            throw new RuntimeException("Error: You may enter 2 to .... values.");
+        }
         Arrays.sort(values);
         int length = values.length;
         return length % 2 != 0 ? (values[length / 2]) : (values[length / 2 ] + values[length / 2 - 1]) / 2.0;
