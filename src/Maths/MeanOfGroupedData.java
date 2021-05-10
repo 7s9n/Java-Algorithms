0ca9b3b172985b8ce8a541d759212b495674f29c
Hussein Sarea <zzzsssx0@gmail.com>
2021-05-10 03:38:46 +0300

    commit

--- src/Maths/MeanOfGroupedData.java
+++ src/Maths/MeanOfGroupedData.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class MeanOfGroupedData {
 
     public static void main (String[] args) {
-        Table dataSet = new Table(20 , 100,8);
+        Table dataSet = new Table(22 , 32,5);
         System.out.println("Mean: " + dataSet.getMean());
     }
 }
@@ -32,19 +32,15 @@ class Pair{
 class Classes{
     private final Pair classes;
     private final int frequency;
-    private final double midPoint;
-    private double frequencyMultipliedByMidPoint;
     public Classes (int lower, int upper , int frequency){
         this.classes = new Pair(lower , upper);
         this.frequency = frequency;
-        this.midPoint = (lower + upper) / 2;
-        this.frequencyMultipliedByMidPoint = this.frequency * this.midPoint;
     }
     public double getFrequency(){
         return this.frequency;
     }
-    public double getFrequencyMultipliedByMidPoint(){
-        return this.frequencyMultipliedByMidPoint;
+    public double getMidPoint(){
+        return (this.classes.getUpper() + this.classes.getLower()) / 2.0;
     }
     @Override
     public String toString(){
@@ -53,15 +49,16 @@ class Classes{
 }
 class Table{
     private List<Classes> classesList;
-
+    private int totalFrequency;
+    private double totalFrequencyMulByMid;
     public Table(int lower , int upper , int numOfClasses){
         classesList = new ArrayList<>();
         this.generateTable(lower , upper , numOfClasses);
+        this.initVariable();
     }
     private void generateTable(int lower , int upper , int numOfClasses){
         int range = upper - lower , classLength = Math.round(range / numOfClasses) , freq = 0;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Length: " + classLength);
         for (int i = 0; i < numOfClasses; i++) {
             System.out.print("Enter frequency of " + lower + " - " + (lower + classLength) + ": ");
             try {
@@ -73,12 +70,24 @@ class Table{
             lower += classLength;
         }
     }
-    public double getMean(){
-        double totalFreq = 0 , totalFreqMulMid = 0;
+    public double getMedian(){
+        int l = 0 ; //lower limit of the median class.
+        int f = 0 ; //frequency of the median class.
+        int F = 0 ; //cumulative frequency of the class preceding the median class.
+        int n = 0 ; //total number of observations / frequency.
+        int h = 0 ; //width of the median class.
         for (Classes c : classesList){
-            totalFreq += c.getFrequency() ;
-            totalFreqMulMid += c.getFrequencyMultipliedByMidPoint();
+
         }
-        return  totalFreqMulMid / totalFreq;
+        return 00.0;
+    }
+    public double getMean(){
+        return totalFrequencyMulByMid / this.totalFrequency;
+    }
+    private void initVariable(){
+        for (Classes c : classesList){
+            totalFrequency += c.getFrequency();
+            totalFrequencyMulByMid += c.getFrequency() * c.getMidPoint();
+        }
     }
 }
