60672251fb8daeb0a3c1b76839ea1725d9e72741
Hussein Sarea <zzzsssx0@gmail.com>
2021-05-23 05:15:31 +0300

    commit

--- src/Strings/ReverseString.java
+++ src/Strings/ReverseString.java
@@ -1,5 +1,23 @@
 package Strings;
 
 public class ReverseString {
+    public static void main (String[] args) {
+        assert reverse("abcd").equals("dcba");
+    }
 
+    /**
+     * Reverse string using two pointer approach.
+     * @param str string to be reversed.
+     * @return reversed str.
+     */
+    public static String reverse(String str){
+        int startIdx = 0 , endIdx = str.length() -1;
+        char [] chars = str.toCharArray();
+        while (startIdx < endIdx){
+            char tmp = chars[endIdx];
+            chars[endIdx--] = chars[startIdx];
+            chars[startIdx++] = tmp;
+        }
+        return new String(chars);
+    }
 }
