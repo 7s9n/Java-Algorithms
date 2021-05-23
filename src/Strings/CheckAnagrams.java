3ba39e276ff6a3308938fb9ed24ac79e44b5d1df
Hussein Sarea <zzzsssx0@gmail.com>
2021-05-23 04:25:10 +0300

    commit

--- src/Strings/CheckAnagrams.java
+++ src/Strings/CheckAnagrams.java
@@ -3,17 +3,36 @@ package Strings;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * An anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ * For example, the word anagram itself can be rearranged into nag a ram,
+ * also the word binary into brainy and the word adobe into abode.
+ * https://en.wikipedia.org/wiki/Anagram
+ */
 public class CheckAnagrams {
     public static void main (String[] args) {
-        System.out.println(isAnagram("funeral","real fun"));
-        System.out.println(isAnagram("restful","fluster"));
-        System.out.println(isAnagram("forty five","over fifty"));
+        assert isAnagram("funeral","real fun");
+        assert isAnagram("restful","fluster");
+        assert isAnagram("forty five","over fifty");
+        assert isAnagram("Satan","Santa");
+        assert isAnagram("New York Times" , "monkeys write");
+        assert isAnagram("Church of Scientology","rich chosen goofy cult");
+        assert isAnagram("eleven plus two" , "twelve plus one");
+        assert isAnagram("elegant man" , "a gentleman");
+        assert !isAnagram("is","are");
     }
+    /**
+     * Check if two strings are anagrams or not , ignoring the case and space character , only numbers and letters
+     * @param firstString the first string
+     * @param secondString the second string
+     * @return {@code true} if two string are anagrams, otherwise {@code false}
+     */
     public static boolean isAnagram(String firstString , String secondString){
         if (!(firstString != null && secondString != null))
-            throw new IllegalArgumentException("null string");
-        firstString = firstString.toLowerCase().replaceAll(" ","");
-        secondString = secondString.toLowerCase().replaceAll(" ","");
+            throw new IllegalArgumentException("Null string");
+        firstString = firstString.toLowerCase().replaceAll("[^\\da-z]","");
+        secondString = secondString.toLowerCase().replaceAll("[^\\da-z]","");
         if ( firstString.length() != secondString.length() || firstString.length() == 0)
             return false;
 
