21adc71ecb187b1d43729c8696972758a5ea3741
Hussein Sarea <zzzsssx0@gmail.com>
2021-05-29 08:19:10 +0300

    commit

--- src/BitManipulation/SetBits.java
+++ src/BitManipulation/SetBits.java
@@ -1,4 +1,15 @@
 package BitManipulation;
 
 public class SetBits {
+    public static int setIthBit(int n , int i){
+        if (i <= 0 || i > 32){
+            throw new IllegalArgumentException("The position must be between 1 and 32");
+        }
+        // ith bit of n is being cleared by this operation.
+        return (1 << i -1) | n;
+    }
+
+    public static void main (String[] args) {
+        System.out.println(setIthBit(65,7));
+    }
 }
--- tests/BitManipulation/ClearBitsTest.java
+++ tests/BitManipulation/ClearBitsTest.java
@@ -14,5 +14,8 @@ class ClearBitsTest {
     void clearIthBit () {
         Assertions.assertEquals(254 ,ClearBits.clearIthBit(255,1));
         Assertions.assertEquals(127 ,ClearBits.clearIthBit(255,8));
+        Assertions.assertEquals(1 , ClearBits.clearIthBit(65,7));
+        Assertions.assertThrows(IllegalArgumentException.class , ()-> ClearBits.clearIthBit(1 , 33));
+        Assertions.assertThrows(IllegalArgumentException.class , ()-> ClearBits.clearIthBit(1 , 0));
     }
 }
