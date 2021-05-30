9b79cac30865b98adc867846b3f0ca092c339fa9
Hussein Sarea <zzzsssx0@gmail.com>
2021-05-30 05:11:40 +0300

    commit

--- src/Conversions/DecimalToAnyBase.java
+++ src/Conversions/DecimalToAnyBase.java
@@ -1,6 +1,10 @@
 package Conversions;
 
 public class DecimalToAnyBase {
+    public static void main (String[] args) {
+        System.out.println(convertToAnyBase(7006362,36));
+        System.out.println(Integer.valueOf("46656",36));
+    }
     /**
      *
      * @param n is a decimal number to be converted.
@@ -9,7 +13,15 @@ public class DecimalToAnyBase {
      */
     public static String convertToAnyBase(int n , int base){
         StringBuilder ans = new StringBuilder();
-        while ()
+        boolean isNegative = n < 0;
+        n = Math.abs(n);
+        while (n > 0){
+            ans.append(getChar(n % base));
+            n /= base;
+        }
+        if (isNegative)
+            ans.append('-');
+        return ans.reverse().toString();
     }
     /**
      * This method produces character value of the input integer and returns it
