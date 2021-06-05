339f1ce9107b16df09e26be1e3a40a852b95956c
Hussein Sarea <zzzsssx0@gmail.com>
2021-06-05 04:49:26 +0300

    commit

--- src/LinearAlgebra/TransposeMatrix.java
+++ src/LinearAlgebra/TransposeMatrix.java
@@ -21,6 +21,25 @@ public class TransposeMatrix {
         }
         return newMatrix;
     }
+
+    /**
+     * In-place matrix transposition, also called in-situ matrix transposition,
+     * is the problem of transposing an N×M matrix in-place in computer memory, ideally with O(1) (bounded) additional storage,
+     * or at most with additional storage much less than NM. Typically,
+     * the matrix is assumed to be stored in row-major order or column-major order
+     * (i.e., contiguous rows or columns, respectively, arranged consecutively).
+     * Performing an in-place transpose (in-situ transpose) is most difficult when N ≠ M, i.e.
+     * for a non-square (rectangular) matrix, where it involves a complicated permutation of the data elements,
+     * with many cycles of length greater than 2. In contrast, for a square matrix (N = M), all of the cycles are of length 1 or 2,
+     * and the transpose can be achieved by a simple loop to swap the upper triangle of the matrix with the lower triangle.
+     * Further complications arise if one wishes to maximize memory locality in order
+     * to improve cache line utilization or to operate out-of-core (where the matrix does not fit into main memory),
+     * since transposes inherently involve non-consecutive memory accesses.
+     *
+     * The problem of non-square in-place transposition has been studied since at least the late 1950s,
+     * and several algorithms are known, including several which attempt to optimize locality for cache,
+     * out-of-core, or similar memory-related contexts.
+     */
     public static void transposeInPlace(int[][] mat){
         if (mat == null)
             return;
@@ -29,14 +48,18 @@ public class TransposeMatrix {
         int r = mat.length , c = mat[0].length;
         if (r != c)
             return;
-
-        for (int i = 0; i < r; i++) {
-            for (int j = i + 1; j < c; j++){
-                int tmp = mat[j][i];
-                mat[j][i] = mat[i][j];
-                mat[i][j] = tmp;
-            }
+        //For a square N×N matrix An,m = A(n,m),
+        // in-place transposition is easy ,
+        // because all of the cycles have length 1 (the diagonals An,n) or length 2 (the upper triangle is swapped with the lower triangle)
+        for (int i = 0; i <= r - 2; i++) {
+            for (int j = i + 1; j <= c - 1; j++)
+                swap(mat , i , j);
         }
+    }
+    private static void swap(int[][] mat , int i , int j){
+        int tmp = mat[j][i];
+        mat[j][i] = mat[i][j];
+        mat[i][j] = tmp;
     }
 
     public static void main (String[] args) {
@@ -57,8 +80,12 @@ public class TransposeMatrix {
         for (int[] row : transpose(mat2))
             System.out.println(Arrays.toString(row));
 
+        for (int[] row : transpose(mat3))
+            System.out.println(Arrays.toString(row));
+
         transposeInPlace(mat3);
         for (int[] row : mat3)
             System.out.println(Arrays.toString(row));
+
     }
 }
