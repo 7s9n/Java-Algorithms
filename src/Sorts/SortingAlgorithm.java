9de9e4b2a889269b980ed2cdf56fe96ad5eeef62
Hussein Sarea <zzzsssx0@gmail.com>
2021-06-06 04:21:36 +0300

    commit

--- src/Sorts/SortingAlgorithm.java
+++ src/Sorts/SortingAlgorithm.java
@@ -3,6 +3,5 @@ package Sorts;
 import java.util.Comparator;
 
 public interface SortingAlgorithm {
-    <T extends Comparable<T> > void sort(T[] array);
-    <T > void sort(T[] array , Comparator<T> c);
+    <T> void sort(T[] array , Comparator<? super T> c);
 }
