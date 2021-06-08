c9a5a168d9057a3fa3ddfe8512998fccfec0ca1a
Hussein Sarea <zzzsssx0@gmail.com>
2021-06-08 06:22:29 +0300

    commit

--- src/LinearAlgebra/MatrixDeterminant.java
+++ src/LinearAlgebra/MatrixDeterminant.java
@@ -32,21 +32,13 @@ public class MatrixDeterminant {
             return a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
         }
         int det = 0;
-        for (int[] ints : matrix) {
-            for (int j = 0; j < n; j++) {
-                int c = ints[j];
-                /*
-                 * Checks if the pivot element is non-zero to be able to do all the calculation.
-                 * If the element is zero, there is no need to do the calculation after all the product will go to zero.
-                 * We won't need to do the partial sum of zero either, since zero added does not influence the result.
-                 */
-                if (c != 0){
-                    int[][] newMatrix = buildMatrix(matrix, j);
-                    int sign = ((j & 1) == 0) ? +1 : -1; // j % 2 == 0 you can use mod operator but & is faster.
-                    det += sign * c * laplace(newMatrix);
-                }
-            } // end of inner loop.
-        } // end of outer loop.
+        for (int i = 0; i < n; i++) {
+            int c = matrix[0][i];
+            if (c != 0){
+                int sign = ((i & 1) == 0) ? +1 : -1;
+                det += sign * matrix[0][i] * det( createSubMatrix(matrix ,0 , i) );
+            }
+        }
         return det;
     }
 
@@ -55,21 +47,21 @@ public class MatrixDeterminant {
      * without the excluded row and column.
      * The Laplace expansion is computationally inefficient for high dimension matrices.
      */
-    private static int[][] buildMatrix(int[][] matrix , int removedColumn){
-        int n = matrix.length;
-        // create a matrix one dimension smaller than the last.
-        int[][] newMatrix = new int[n -1][n - 1];
-        int currentRow = 0; // to track the row of the new matrix
-        for (int i = 1; i < n; ++i , ++currentRow) {
-            int currentCol = 0;
+    private static int[][] createSubMatrix(int[][] mat , int excludingRow , int excludingCol){
+        int n = mat.length;
+        int[][] newMatrix = new int[n - 1][n - 1];
+        int rPtr = -1;
+        for (int i = 0; i < n; i++) {
+            if (i == excludingRow)continue;
+            ++rPtr;
+            int cPtr = -1;
             for (int j = 0; j < n; j++) {
-                if (j == removedColumn)continue;
-                newMatrix[currentRow][currentCol++] = matrix[i][j];
+                if (j == excludingCol)continue;
+                newMatrix[rPtr][++cPtr] = mat[i][j];
             } // end of inner loop
         } // end of outer loop
-        //printMatrix(newMatrix); // to see the new matrix after removing  .
         return newMatrix;
-    } // end of buildMatrix.
+    } // end of createSubMatrix
     private static void printMatrix(int[][] mat){
         for (int[] row : mat)
             System.out.println(Arrays.toString(row));
@@ -120,7 +112,6 @@ public class MatrixDeterminant {
                 {2 , 1 , 3 , 8 , 9 , 1 , 4 , 3},
                 {6 , 1 , 6 , 7 , 9 , 1 , 4 , 2}
         }; // det(mat6) = -39240 , mat(8 * 8)
-        // from mat7 this will take some time to get the result
         int[][] mat7 = new int[][]{
                 {1 , 1 , 9 , 3 , 1 , 2 , 3 , 9 , 1},
                 {9 , 1 , 8 , 4 , 2 , 3 , 1 , 8 , 2},
@@ -131,7 +122,7 @@ public class MatrixDeterminant {
                 {2 , 1 , 3 , 8 , 9 , 1 , 4 , 3 , 7},
                 {6 , 1 , 6 , 7 , 9 , 1 , 4 , 2 , 8},
                 {9 , 8 , 7 , 4 , 3 , 3 , 4 , 2 , 9}
-        }; // det(mat7) = 1910870 , mat( 9 * 9)  Elapsed time: 1012 seconds , 16.86667 minutes
+        }; // det(mat7) = 1910870 , mat( 9 * 9)
         int[][] mat0 = new int[][]{
                 {1 , 2 , 4 , 8 , 6 , 3 , 4 , 8 , 0 , 2},
                 {2 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 1},
@@ -143,7 +134,8 @@ public class MatrixDeterminant {
                 {1 , 2 , 4 , 0 , 0 , 0 , 0 , 3 , 5 , 2},
                 {1 , 1 , 0 , 4 , 5 , 0 , 0 , 4 , 2 , 1},
                 {1 , 0 , 0 , 0 , 9 , 0 , 0 , 1 , 1 , 6}
-        }; // det(mat0) = --- , 2 hour and the program's still running... I'll turn it off , lol
+        }; // det(mat0) = 17265530
+
         Instant start = Instant.now();
         System.out.println(det(mat1));
         System.out.println(det(mat2));
